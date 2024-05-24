8ad286ef3327b900623ff196953e2d54f83886a0
Shankar Halemani <shankarlinghalemani23@gmail.com>
2024-05-24 13:18:14 +0530

    Update PigTailDiceGame.java

--- PigTailDiceGame.java
+++ PigTailDiceGame.java
@@ -6,11 +6,14 @@ import java.util.Scanner;
 public class PigTailDiceGame {
 
 	public static void main(String[] args) {
+
+		//initialization
 		Scanner scanner = new Scanner(System.in);
 		Random random = new Random();
 		int sum = 0;
 		int totalTurns = 0;
 
+		//loop runs till sum of dice becomes 20 or more than 20
 		while (sum < 20) {
 			totalTurns++;
 			System.out.println("TURN : " + totalTurns);
@@ -19,6 +22,8 @@ public class PigTailDiceGame {
 			String action = scanner.nextLine();
 
 			int scoreForTurn = 0;
+
+			//loop runs till player rolls 1 or wants to hold
 			while (action.equalsIgnoreCase("Roll")) {
 				int dice = random.nextInt(6) + 1;
 				System.out.println("Dice : " + dice);
@@ -34,11 +39,13 @@ public class PigTailDiceGame {
 				}
 			}
 
+			//score of each turn
 			System.out.println("Score for the turn : " + scoreForTurn);
 			sum += scoreForTurn;
 			System.out.println();
 		}
 
+		// Total score of player
 		System.out.println("Total score : " + sum);
 		System.out.println("You finished in " + totalTurns + " turns!");
 		System.out.println("Game over!");
