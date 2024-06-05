447bb6341cb5c2613f649ca63c12fa4ddc19b724
Shankar Halemani <shankarlinghalemani23@gmail.com>
2024-06-05 21:58:47 +0530

    Update SecondLargestElementOfArray.java

--- SecondLargestElementOfArray.java
+++ SecondLargestElementOfArray.java
@@ -4,49 +4,61 @@ import java.util.Scanner;
 
 public class SecondLargestElementOfArray {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// Initialization
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter size of array");
-		int size = scanner.nextInt();
-		int[] arr = new int[size];
+        // Initialization
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter size of array");
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
 
-		// Taking input from user
-		System.out.println("Enter array elements");
-		for (int i = 0; i < size; i++) {
-			arr[i] = scanner.nextInt();
-		}
+        // Taking input from user
+        System.out.println("Enter array elements");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
 
-		// Exit if size of array is less than 2
-		if (size < 2) {
-			System.out.println("There is no second largest element");
-		}
+        // Exit if size of array is less than 2
+        if (size < 2) {
+            System.out.println("There is no second largest element");
+        } else {
+            // Sort the array using selection sort
+            selectionSort(arr);
 
-		else {
+            // Find the second largest element in the sorted array
+            int maxElement = arr[size - 1];
+            int secondLargestElement = Integer.MIN_VALUE;
 
-			int maxElement = Integer.MIN_VALUE;
-			int secondLargestElement = Integer.MIN_VALUE;
+            for (int i = size - 2; i >= 0; i--) {
+                if (arr[i] != maxElement) {
+                    secondLargestElement = arr[i];
+                    break;
+                }
+            }
 
-			// Find the second maximum element in the array
-			for (int i = 0; i < size; i++) {
+            if (secondLargestElement == Integer.MIN_VALUE) {
+                System.out.println("There is no second largest element");
+            } else {
+                System.out.println("Second largest element: " + secondLargestElement);
+            }
+        }
+        scanner.close();
+    }
 
-				//finding largest element in array
-				if (arr[i] > maxElement) {
-					secondLargestElement = maxElement;
-					maxElement = arr[i];
-				}
-
-				//finding second largest element in array
-				else if (arr[i] != maxElement && arr[i] > secondLargestElement) {
-					secondLargestElement = arr[i];
-				}
-			}
-
-			System.out.println("Second largest element : " + secondLargestElement);
-
-		}
-		scanner.close();
-	}
-
+    // Selection sort method
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap the found minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
 }
