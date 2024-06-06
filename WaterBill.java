7ad03ed83460bc4d4cf69e2b368c1ed76d671e01
Shankar Halemani <shankarlinghalemani23@gmail.com>
2024-06-06 17:45:43 +0530

    Update WaterBill.java

--- WaterBill.java
+++ WaterBill.java
@@ -4,26 +4,23 @@ import java.util.Scanner;
 
 public class WaterBill {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the total number of units used");
-		int numOfUnits = sc.nextInt();
-		int meter_charge = 75;
-		int charge;
-		if(numOfUnits <=100) {
-			charge = numOfUnits * 5;
-		}
-		else if(numOfUnits<=250) {
-				charge = numOfUnits * 10;
-			}
-		else {
-			charge = numOfUnits * 20;
-			}
-		
-		int total_water_bill = charge + meter_charge;
-		System.out.println("Tota water bill : " + total_water_bill);
-		sc.close();
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the total number of units used");
+        int numOfUnits = sc.nextInt();
+        int meterCharge = 75;
+        int charge = 0;
 
+        if (numOfUnits <= 100) {
+            charge = numOfUnits * 5;
+        } else if (numOfUnits <= 250) {
+            charge = numOfUnits * 10;
+        } else (numOfUnits > 250) {
+            charge = numOfUnits * 20;
+        }
+
+        int totalWaterBill = charge + meterCharge;
+        System.out.println("Total water bill : " + totalWaterBill);
+        sc.close();
+    }
 }
